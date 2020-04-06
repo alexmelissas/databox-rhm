60ed197a6b7e56eaa1ffdb24e0a2f5de4cfb2cb6
Alex <psyam19@nottingham.ac.uk>
2020-04-06 10:36:52 +0100

    ok

--- server/server.js
+++ server/server.js
@@ -255,7 +255,8 @@ app.post('/store', (req,res) =>{
     var data = Buffer.from(req.body.data); //dont try to decrypt - crashes cause doesnt have peerkey
     var checksum = Buffer.from(req.body.checksum);
   
-    sqlConnection.query("INSERT INTO databoxrhm (pin, checksum, data, ttl) VALUES (?, ?,?, ?);",
+    sqlConnection.query("INSERT INTO databoxrhm (pin, checksum, data, ttl)"
+                        +"VALUES (?, ?,?, ?);",
                          [pin,checksum,data,7], function (err, result) { 
       if (result!=null) console.log('[+] Added data:\n      PIN:', pin,
                                     '\n checksum:', checksum,
