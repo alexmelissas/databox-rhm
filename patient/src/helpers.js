ae7904580c5a6a67ab502275b2b7253918f6c262
Alex <psyam19@nottingham.ac.uk>
2020-04-17 14:59:00 +0100

    ip

--- patient/src/helpers.js
+++ patient/src/helpers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const HKDF = require('hkdf');
 const request = require('request');
 
-const SERVER_IP = '35.178.207.64';
+const SERVER_IP = '35.178.184.151';
 const TLS_PORT = 8000;
 const SERVER_URI = "https://"+SERVER_IP+":"+TLS_PORT+"/";
 const TURN_USER = 'alex';
